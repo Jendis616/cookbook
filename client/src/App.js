18bc238f698d672d9d8f6df996646a6706470ff5
Jendis616 <j.lukepudis@gmail.com>
2023-10-16 14:35:34 +0200

    Add Cookbook to git

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import '../public/Cook_Book.jpg' Cook_Book;
+
+const image = {Cook_Book};
+
+function App() {
+  return (
+    <div className="App">
+      <h1></h1>
+    </div>
+  );
+}
+
+export default App;
